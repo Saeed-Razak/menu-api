6194995c52c92ba5aac5e96049a7080be1c0f830
Saeed-Razak <saeedabdulrazak95@gmail>
2022-04-14 14:30:52 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const morgan = require("morgan")
+require("dotenv").config()
+const connectDB = require('./config/connectDB')
+const foodRoute = require("./routes/foodRoute")
+
+
+
+const app = express()
+
+connectDB()
+//middlewares
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(foodRoute)
+
+
+const PORT =process.env.PORT||8000
+
+//Home route
+app.get("/", (req, res )=>{
+    res.json("Welcome to my Menu API")
+})
+
+app.listen(PORT,(req, res)=>{
+    console.log("server is working!!!")
+})
